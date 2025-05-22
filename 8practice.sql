6628cf81afcc3d47cc6b71be78a5d580e995d39b
Emon <emon.mhk69@gmail.com>
2025-05-23 01:31:49 +0600

    8.1 done

--- 8practice.sql
+++ 8practice.sql
@@ -22,8 +22,31 @@ ALTER Table student
 
 -- Add a UNIQUE constraint to student_email.
 ALTER TABLE student
+    ADD CONSTRAINT UNIQUE_STUDENT_EMAIL UNIQUE(student_email)
 
 -- Add a PRIMARY KEY to a new table named courses.
-
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100),
+    credits INT
+);
 
 -- Drop a column from any existing table.
+ALTER TABLE student
+    DROP COLUMN last_login
+
+
+
+-- Filtering & Logical Operations (Based on 8-5, 8-7, 8-8) Write a query to find all students who have a score greater than 80 and not null.
+
+
+-- Use the NOT operator to exclude students from a specific department.
+
+
+-- Fetch students whose names start with ‘A’ using LIKE and ILIKE.
+
+
+-- Select all students whose age is between 18 and 25.
+
+
+-- Retrieve rows using IN for a specific set of roll numbers.
